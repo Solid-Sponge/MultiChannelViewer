fe7bf029d88bd2c8b33f455657a79860f1d96b14
Michael <ucsipro@hotmail.com>
2016-03-18 16:04:56 -0400

    Fixed crucial bug regarding NIR filter
    Changed vector type in filter from short to unsigned short. This was
    previously causing crashes whenever the NIR camera read values too high
    due to overflow (negative values).

--- camera.cpp
+++ camera.cpp
@@ -163,7 +163,7 @@ void Camera::capture()
         unsigned short* rawPtr = static_cast<unsigned short*>(Frames[0].ImageBuffer);
         unsigned char* filter = new unsigned char[Frames[0].ImageSize];
         unsigned short* filterPtr = reinterpret_cast<unsigned short*>(filter);
-        QVector<short> window(8);
+        QVector<unsigned short> window(8);
         for (int i = 1; i < Frames[0].Height - 1; i++)
         {
             for (int j = 1; j < Frames[0].Width - 1; j++)
